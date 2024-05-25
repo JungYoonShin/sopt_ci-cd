e20a2814e6cde2456489abc7f98f2b7a6208a6b6
JungYoonShin <sju08227@naver.com>
2024-05-25 17:22:46 +0900

    .

--- src/main/java/com/sopt/seminar/controller/ProfileController.java
+++ src/main/java/com/sopt/seminar/controller/ProfileController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
-
 @RestController
 @RequiredArgsConstructor
 public class ProfileController {
